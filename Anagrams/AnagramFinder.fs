bf213145a7a20109b90a3c86fcf2e14f7e3b6d4c
adridevparis <mama0304>
2017-02-28 08:54:02 +0100

    First commit for anagrams finder

---
+++ Anagrams/AnagramFinder.fs
@@ -0,0 +1,12 @@
+﻿module AnagramFinder
+
+let toto = "test"
+
+let addHash crc c = crc + c.GetHashCode();
+
+let iterateOverString str = 
+    let mutable crc = 0
+    Seq.iter (fun c -> crc <- addHash crc c) str
+    crc
+
+iterateOverString toto
